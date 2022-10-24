59a814c3bac8b231828d2779744ef53664aa3751
wizexplorer <93021399+wizexplorer@users.noreply.github.com>
2022-10-23 18:40:16 -0700

    Add files via upload

---
+++ factorial.py
@@ -0,0 +1,10 @@
+print("|-------------------------EXPERIMENT NO.12-----------------------------|")
+print("|                      FACTORIAL OF A NUMBER                           |")
+print("*"*72)
+N=eval(input("                  Enter the number   : "))
+piggy=1
+for i in range(1,N+1):
+    piggy=piggy*i
+print("|----------------------------------------------------------------------|")
+print("|                 Factorial of " ,N, "is :",piggy,"                            |")
+print("|----------------------------------------------------------------------|")
